cb1c4eb7faf1d27e49f824061d12b338d6b067af
algosipdahack <sophia5460@ewhain.net>
2022-09-01 10:17:54 +0900

    soyeon

--- 22-08-31/6236-soyeon.py
+++ 22-08-31/6236-soyeon.py
@@ -1,7 +1,8 @@
 '''
 https://www.acmicpc.net/problem/6236
-이분탐색
-
+이분탐색의 정수를 보여준 문제. 
+answer를 따로 파서 답을 출력하는 데 어려움이 있었다.
+min하고 max값을 설정하는 데에 따라 답이 달라짐
 '''
 import sys
 N,M = map(int,sys.stdin.readline().strip().split())
