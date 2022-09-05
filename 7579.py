afaedacf5387af091e55e444b73686b72f9b2e74
jonghyukleee <jh5253krkr@naver.com>
2022-09-05 10:28:22 +0900

    16198-jh

--- 7579.py
+++
@@ -1,5 +0,0 @@
-import sys
-N,M = map(int,sys.stdin.readline().strip().split())
-app = list(map(int,sys.stdin.readline().strip().split()))
-memory = list(map(int,sys.stdin.readline().strip().split()))
-answer = [[0]*
